312d32beb518bc39f1abc531972e4fb1470755dd
mamin <mamin.papa0@gmail.com>
2023-11-05 15:35:04 +1000

    добавить паддинг еще надо

--- Enemy.py
+++ Enemy.py
@@ -4,4 +4,3 @@ from Character import Character
 class Enemy(Character):
     def __init__(self, x, y, start_image, images, group_all_sprite):
         super().__init__(x, y, start_image, images, group_all_sprite)
-#чтоб добавить xd
