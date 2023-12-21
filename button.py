6d49cfeed0cca2718c69412bd0a801c0c10feae5
mamin <mamin.papa0@gmail.com>
2023-12-21 11:38:30 +1000

    ну че, осталось только мапу сделать

--- button.py
+++ button.py
@@ -18,10 +18,8 @@ class Button:
         screen.blit(self.text, self.text_rect)
 
     def checkForInput(self, position):
-        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
-                                                                                          self.rect.bottom):
-            return True
-        return False
+        return position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                          self.rect.bottom)
 
     def changeColor(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
